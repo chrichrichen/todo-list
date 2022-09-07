753753b1fdf3043eedc741bd1bafcbdc8cdfa7ec
Christal Chen <christalchen1209@gmail.com>
2022-09-07 19:19:53 +0800

    feat: add todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -20,12 +21,24 @@ db.once('open',()=>{
 
 app.engine('handlebars',exphbs({defaultLayout :'main'}))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
   Todo.find()
   .lean()
   .then(todos => res.render('index',{todos : todos}))
   .catch(error => console.error(error))
+})
+
+app.get('/todos/new',(req,res)=>{
+  return res.render('new')
+})
+
+app.post('/todos' , (req,res)=>{
+  const name = req.body.name
+  return Todo.create({name})
+  .then(()=> res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
