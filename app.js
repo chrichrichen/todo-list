e4d5065b1780f389f9961b7609739e06640feee3
Christal Chen <christalchen1209@gmail.com>
2022-09-13 11:52:08 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const routes = require('./routes')
 const app = express()
 const port = 3000
 
@@ -24,62 +25,11 @@ app.engine('handlebars',exphbs({defaultLayout :'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  Todo.find()
-  .lean()
-  .sort({_id:'asc'})
-  .then(todos => res.render('index',{todos : todos}))
-  .catch(error => console.error(error))
-})
 
-app.get('/todos/new',(req,res)=>{
-  return res.render('new')
-})
 
-app.post('/todos' , (req,res)=>{
-  const name = req.body.name
-  return Todo.create({name})
-  .then(()=> res.redirect('/'))
-  .catch(error => console.log(error))
-})
 
-app.get('/todos/:id',(req,res)=>{
-  const id = req.params.id
-  return Todo.findById(id)
-  .lean()
-  .then((todo)=> res.render('detail',{todo}))
-  .catch(error=>(console.log(error)))
-})
-
-app.get('/todos/:id/edit',(req,res)=>{
-  const id = req.params.id
-  return Todo.findById(id)
-  .lean()
-  .then((todo)=>res.render('edit',{todo}))
-  .catch(error=> console.log(error))
-})
-
-app.put('/todos/:id/',(req,res)=>{
-  const id = req.params.id
-  const {name,isDone} = req.body
-  return Todo.findById(id)
-  .then(todo =>{
-    todo.name = name
-    todo.isDone = isDone === 'on'
-    return todo.save()
-  })
-  .then(()=> res.redirect(`/todos/${id}`))
-  .catch(error=>console.log(error))
-})
-
-app.delete('/todos/:id/',(req,res)=>{
-  const id = req.params.id
-  return Todo.findById(id)
-  .then(todo=> todo.remove())
-  .then(()=>res.redirect('/'))
-  .catch(error=>console.log(error))
-})
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
